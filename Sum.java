678240e1d0f7aeb1872e1f930d3377d2e7e06e89
Pratiksha Danake <danakepratiksha1020@gmail.com>
2023-09-08 14:17:45 +0530

    v6

--- Sum.java
+++ Sum.java
@@ -6,6 +6,8 @@ public static void main(String args[])
 System.out.print("hello world");
 System.out.print("hello ");
 System.out.print("hiiii ");
+System.out.print("hiiii hiiiii hiiiiii hiiiiii ");
+
 }
 }
 
